ce18c58ee8ef534161aac2b2b532e77163a9dff2
VeladandiSaiCharan <veldandisaichar@gmail.com>
2026-09-01 22:28:26 +0530

    Completed LAB 16

---
+++ FreeCodeCamp JS/JS Fundamental review/LAB 16/script.js
@@ -0,0 +1,17 @@
+function sumFibs(num) {
+    let a = 0;
+    let b = 1;
+    let sum = 0;
+
+    while (a <= num) {
+        if (a % 2 !== 0) {
+            sum += a;
+        }
+
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return sum;
+}
