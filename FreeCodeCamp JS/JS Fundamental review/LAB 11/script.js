7b2e355bd7d6edd24efdecbfd3edaa9bd27f196c
VeladandiSaiCharan <veldandisaichar@gmail.com>
2026-09-01 22:06:48 +0530

    Completed LAB 11

---
+++ FreeCodeCamp JS/JS Fundamental review/LAB 11/script.js
@@ -0,0 +1,15 @@
+function uniteUnique(...arrays) {
+    const result = [];
+
+    for (let i = 0; i < arrays.length; i++) {
+        for (let j = 0; j < arrays[i].length; j++) {
+
+            if (!result.includes(arrays[i][j])) {
+                result.push(arrays[i][j]);
+            }
+
+        }
+    }
+
+    return result;
+}
